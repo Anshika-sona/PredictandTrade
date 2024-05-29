c940cde11d01a2e8b79ba481c3de6a33f0bd3929
Anshika <97749793+Anshika-sona@users.noreply.github.com>
2024-05-29 09:08:28 +0530

    Update PredictandTrade.py

--- PredictandTrade.py
+++ PredictandTrade.py
@@ -30,7 +30,7 @@ add_meta_tag()
 # Sidebar Section Starts Here
 today = date.today()  # today's date
 st.write('''# PredictandTrade ''')  # title
-st.sidebar.image("Images/StockStreamLogo1.png", width=250,
+st.sidebar.image("StockStreamLogo1.png", width=250,
                  use_column_width=False)  # logo
 st.sidebar.write('''# PredictandTrade ''')
 
